ba52a70c5ae14d553ab454277c3261a5506251b6
hantulaut88 <faldhinbilly@gmail.com>
2024-12-11 05:45:26 +0700

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // Daftar pertanyaan dan jawaban yang benar
 const questions = [
     { question: "Apa warna langit?", answer: "biru" },
-    { question: "Berapa banyak bulan di bumi?", answer: "1" },
-    { question: "Siapa presiden pertama Indonesia?", answer: "Soekarno" },
-    { question: "Apa ibu kota Jepang?", answer: "Tokyo" },
-    { question: "Siapa penemu telepon?", answer: "Alexander Graham Bell" },
-    { question: "Apa nama planet terdekat dengan matahari?", answer: "Merkurius" },
-    { question: "Berapa banyak negara di dunia?", answer: "195" },
-    { question: "Siapa pencipta Facebook?", answer: "Mark Zuckerberg" },
-    { question: "Apa warna darah manusia?", answer: "merah" },
-    { question: "Siapa yang menulis Harry Potter?", answer: "J.K. Rowling" },
 ];
 
 let currentQuestion = 0;
